a963e14d47d853684fa7aad2887bb67547d406e0
João Marques <joaomarques.wn@gmail.com>
2024-10-25 11:31:42 +0100

    add a img of my cat

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Image from 'next/image';
+
 function Home() {
-    return <h1>Test 3</h1>
+    return(
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Meet My Cat!</h1>
+      <Image src="/my_cat.jpg" width={500} height={500} />
+    </div>
+    )
 }
 
 export default Home
